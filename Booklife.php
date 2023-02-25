68dd174ec70f2b1c3a553448b09a1a2887eb90de
RiskiRishiii <126314803+RiskiRishiii@users.noreply.github.com>
2023-02-25 12:50:58 +0530

    Create Booklife.php

---
+++ Booklife.php
@@ -0,0 +1,39 @@
+<section id="categories">
+  <h2>Categories</h2>
+  <p>Explore books in your favourite genre</p>
+  <div class="category">
+    <a href="#">Fiction</a>
+    <a href="#">Non-Fiction</a>
+    <a href="#">Romance</a>
+    <a href="#">Thriller</a>
+  </div>
+  <div class="category">
+    <a href="#">Mystery</a>
+    <a href="#">Horror</a>
+    <a href="#">Science Fiction</a>
+    <a href="#">Biographies</a>
+  </div>
+</section>
+
+<section id="featured-books">
+  <h2>Featured Books</h2>
+  <p>Discover our handpicked selection of featured books</p>
+  <div class="book">
+    <img src="book1.jpg" alt="Book Cover">
+    <h3>The Alchemist</h3>
+    <p>Paulo Coelho</p>
+    <a href="#" class="cta">Buy Now</a>
+  </div>
+  <div class="book">
+    <img src="book2.jpg" alt="Book Cover">
+    <h3>The Girl on the Train</h3>
+    <p>Paula Hawkins</p>
+    <a href="#" class="cta">Buy Now</a>
+  </div>
+  <div class="book">
+    <img src="book3.jpg" alt="Book Cover">
+    <h3>The Silent Patient</h3>
+    <p>Alex Michaelides</p>
+    <a href="#" class="cta">Buy Now</a>
+  </div>
+</section>
